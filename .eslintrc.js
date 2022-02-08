fd71d55217d658777391ca6edf140bbddeb0f995
PARKER\Parker <parkers@mail.ru>
2022-02-08 12:26:02 +0300

    Add sanity-check.yml

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,36 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-        "jest/globals": true,
+  env: {
+    browser: true,
+    es2021: true,
+    "jest/globals": true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  plugins: ["jest", "@typescript-eslint"],
+  rules: {
+    "import/extensions": [
+      "error",
+      {
+        ts: "never",
+        js: "never",
+      },
+    ],
+    "max-len": [
+      "error",
+      {
+        ignoreComments: true,
+      },
+    ],
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".ts", ".js"],
+      },
     },
-    extends: [ "airbnb-base", "prettier" ],
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-        ecmaVersion: 13,
-        sourceType: "module"
-    },
-    plugins: [ "jest", "@typescript-eslint" ],
-    rules: {
-    },
-    settings: {
-        "import/resolver": {
-          node: {
-            extensions: [".ts", ".js"],
-          },
-        },
-    },
+  },
 };
--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,21 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.[tj]sx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
 };
