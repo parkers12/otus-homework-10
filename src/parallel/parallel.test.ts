fe050b7780e9ca7e9e53c9e999b7b1865f2cb23c
PARKER\Parker <parkers@mail.ru>
2022-02-24 11:17:45 +0300

    fix bug parallel

--- src/parallel/parallel.test.ts
+++ src/parallel/parallel.test.ts
@@ -1,29 +1,38 @@
 import Parallel from "./parallel";
 
-jest.useFakeTimers();
-
 describe("Test function Parallel", () => {
-    test("Return array", async () => {
-        jest.setTimeout(70000);
-        const runner = new Parallel(2);
-        const arg1 = () => new Promise((resolve) => {
-            setTimeout(resolve, 1000, 1);
-        });
-        const arg2 = () => new Promise((resolve) => {
-            setTimeout(resolve, 4000, 2);
-        });
-        const arg3 = () => new Promise((resolve) => {
-            setTimeout(resolve, 2000, 3);
-        });
-        const arg4 = () => new Promise((resolve) => {
-            setTimeout(resolve, 9000, 4);
-        });
-        const arg5 = () => new Promise((resolve) => {
-            setTimeout(resolve, 3000, 5);
+    const jobs: (() => Promise<number>)[] = [
+        () => new Promise((resolve) => {
+            setTimeout(resolve, 10, 1);
+        }),
+        () => new Promise((resolve) => {
+            setTimeout(resolve, 40, 2);
+        }),
+        () => new Promise((resolve) => {
+            setTimeout(resolve, 20, 3);
+        }),
+        () => new Promise((resolve) => {
+            setTimeout(resolve, 90, 4);
+        }),
+        () => new Promise((resolve) => {
+            setTimeout(resolve, 30, 5);
         })
+    ];
 
+    test("It's a function", async () => {
+        const runner = new Parallel(2);
         expect(typeof runner.jobs).toBe("function");
-          return expect(runner.jobs(arg1, arg2, arg3, arg4, arg5))
-            .resolves.toBe([1, 3, 2, 5, 4]);
+    });
+
+    test("Return array 2 streams", async () => {
+        const runner = new Parallel(2);
+        const result = await runner.jobs(...jobs);
+        return expect(result).toStrictEqual([1, 3, 2, 5, 4]);
+    });
+
+    test("Return array 5 streams", async () => {
+        const runner = new Parallel(5);
+        const result = await runner.jobs(...jobs);
+        expect(result).toStrictEqual([1, 3, 5, 2, 4]);
     });
 });
--- src/parallel/parallel.ts
+++ src/parallel/parallel.ts
@@ -1,5 +1,5 @@
 type Task = () => Promise<number>
-
+type Resolve = (arg: any) => void;
 export default class Parallel {
     private tasks: Task[] = [];
 
@@ -10,14 +10,14 @@ export default class Parallel {
     }
 
     public jobs(...tasks: Task[]) {
-        this.tasks = tasks;
-        const out: number[] = []
+        this.tasks = [...tasks];
+        const out: number[] = [];
         return new Promise((resolve) => {
             this.fillStack(out, resolve)
         })
     }
 
-    private fillStack(out: number[], resolve: (v: any) => void) {
+    private fillStack(out: number[], resolve: Resolve) {
         while (this.tasks.length > 0 && this.taskRunning <= this.countStream) {
             const fn = this.tasks.splice(0, 1)
             this.taskRunning += 1;
@@ -26,7 +26,6 @@ export default class Parallel {
                     out.push(v);
                     this.taskRunning -= 1;
                     this.fillStack(out, resolve);
-                    return v;
                 })
         }
 
@@ -35,29 +34,3 @@ export default class Parallel {
         }
     }
 }
-
-const runner = new Parallel(2);
-
-export async function log() {
-    console.log(
-        await runner.jobs(
-            () => new Promise((resolve) => {
-                setTimeout(resolve, 1000, 1);
-            }),
-            () => new Promise((resolve) => {
-                setTimeout(resolve, 4000, 2);
-            }),
-            () => new Promise((resolve) => {
-                setTimeout(resolve, 2000, 3);
-            }),
-            () => new Promise((resolve) => {
-                setTimeout(resolve, 9000, 4);
-            }),
-            () => new Promise((resolve) => {
-                setTimeout(resolve, 3000, 5);
-            })
-        )
-    );
-}
-
-log();
