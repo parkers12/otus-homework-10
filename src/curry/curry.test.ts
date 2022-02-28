f4a7a7351f552fec1b79956fb4e0163641adab76
PARKER\Parker <parkers@mail.ru>
2022-02-28 23:55:03 +0300

    Add types function curry

--- src/curry/curry.test.ts
+++ src/curry/curry.test.ts
@@ -1,34 +1,33 @@
-import { curry, sum } from "./curry";
+import { curry } from "./curry";
 
 describe("Test function hof", () => {
-    let hof: (...args: number[]) => any;
-    beforeEach(() => {
-        hof = curry(sum);
+    const sum = (
+        a: number,
+        b: number,
+        c: number,
+        d: number,
+        e: number
+    ) => a + b + c + d + e;
+
+    const hof = curry(sum);
+
+    test("Get an array of live cells", () => {
+        expect(hof(1)(2)(3)(4)(5)).toBe(15);
     });
 
     test("Get an array of live cells", () => {
-        expect(hof(1)(2)(3)(4)(5)()).toBe(15);
+        expect(hof(2, 3, 4)(5, 6)).toBe(20);
     });
 
     test("Get an array of live cells", () => {
-        expect(hof(2, 3, 4)(5, 6)()).toBe(20);
+        expect(hof(3, 4)(5, 6)(7)).toBe(25);
     });
 
     test("Get an array of live cells", () => {
-        expect(hof(3, 4)(5, 6)(7)()).toBe(25);
+        expect(hof(4, 5)(6)(7, 8)).toBe(30);
     });
 
     test("Get an array of live cells", () => {
-        expect(hof(4, 5)(6)(7, 8)()).toBe(30);
-    });
-
-    test("Get an array of live cells", () => {
-        expect(hof(5)(6)(7)(8)(9)()).toBe(35);
-    });
-});
-
-describe("Test function totalSum", () => {
-    test("Get an array of live cells", () => {
-        expect(sum(1,2,3,4,5)).toBe(15);
+        expect(hof(5)(6)(7)(8)(9)).toBe(35);
     });
 });
--- src/curry/curry.ts
+++ src/curry/curry.ts
@@ -1,20 +1,57 @@
-export const sum = (...args: number[]) =>
-  args.reduce((acc, cur) => acc + cur, 0);
-
-export function curry(func: Function) {
-  let argsArray: number[] = [];
-  return function curried(this: any, ...args: number[]): any {
-    let res;
-    if (args.length === 0) {
-      res = func(...argsArray);
-    } else {
-      argsArray = argsArray.concat(args);
-      res = function pass(...args2: any[]) {
-        return curried.apply(this, args2);
-      }
-    }
-    return res;
-  }
+interface CurriedFunction1<T, R> {
+  (t1: T): R;
 }
 
-export const hof = curry(sum);
+interface CurriedFunction2<T, R> {
+  (t1: T, t2: T): R;
+  (t1: T): CurriedFunction1<T, R>;
+}
+
+interface CurriedFunction3<T, R> {
+  (t1: T, t2: T, t3: T): R;
+  (t1: T, t2: T): CurriedFunction1<T, R>;
+  (t1: T): CurriedFunction2<T, R>;
+}
+
+interface CurriedFunction4<T, R> {
+  (t1: T, t2: T, t3: T, t4: T): R;
+  (t1: T, t2: T, t3: T): CurriedFunction1<T, R>;
+  (t1: T, t2: T): CurriedFunction2<T, R>;
+  (t1: T): CurriedFunction3<T, R>;
+}
+
+interface CurriedFunction5<T, R> {
+  (t1: T, t2: T, t3: T, t4: T, t5: T): R;
+  (t1: T, t2: T, t3: T, t4: T): CurriedFunction1<T, R>;
+  (t1: T, t2: T, t3: T): CurriedFunction2<T, R>;
+  (t1: T, t2: T): CurriedFunction3<T, R>;
+  (t1: T): CurriedFunction4<T, R>;
+}
+
+export type CurriedFunction<T, R> = T extends [R, R, R, R, R]
+  ? CurriedFunction5<R, R>
+  : T extends [R, R, R, R]
+  ? CurriedFunction4<R, R>
+  : T extends [R, R, R]
+  ? CurriedFunction3<R, R>
+  : T extends [R, R]
+  ? CurriedFunction2<R, R>
+  : T extends [R]
+  ? CurriedFunction1<R, R>
+  : never;
+
+export function curry<T extends any[], R>(
+  func: (...args: T) => R
+): CurriedFunction<T, R>;
+
+export function curry(func: (...args: any[]) => any): any {
+  const arrLn = func.length;
+  return function curried(...args: any[]) {
+    if (args.length >= arrLn) {
+      return func(...args);
+    }
+    return  function pass(...args2: any[]) {
+      return curried(...args.concat(args2));
+    }
+  }
+}
